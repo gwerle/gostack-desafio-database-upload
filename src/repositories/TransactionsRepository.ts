c342f990ff8b4df0ff3ecdd0d7dbd44ee6cce2de
Gustavo Werle <gustavo.werle2@gmail.com>
2020-06-30 02:37:52 -0300

    feat: add CRUD

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,30 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const listAll = await this.find();
+    return listAll.reduce(
+      (accumulator, currentValue) => {
+        const income =
+          currentValue.type === 'income'
+            ? accumulator.income + Number(currentValue.value)
+            : accumulator.income;
+        const outcome =
+          currentValue.type === 'outcome'
+            ? accumulator.outcome + Number(currentValue.value)
+            : accumulator.outcome;
+
+        return {
+          income,
+          outcome,
+          total: income - outcome,
+        };
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
   }
 }
 
